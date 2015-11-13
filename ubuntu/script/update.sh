0eb8f4c822c5197cb69d4f838126877881fd9527
Florian Uhlig <f.uhlig@gsi.de>
2015-11-13 09:12:16 +0100

    Merge templates for desktop and server installation. The version is choosen using a command line parameter.

---
+++ ubuntu/script/update.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Update the box
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install linux-headers-$(uname -r) 
+
+if [ "$build_type" = "desktop" ]; then
+  apt-get -y install ubuntu-desktop
+fi
