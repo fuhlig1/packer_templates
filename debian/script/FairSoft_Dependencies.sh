e8e97937318aedb0665731046c53d0a3f96ad56e
Florian Uhlig <f.uhlig@gsi.de>
2015-11-16 16:41:28 +0100

    Put all apt commands into noninteractive mode. Shutdown virtual machine explicitely when using Debian7.

--- debian/script/FairSoft_Dependencies.sh
+++ debian/script/FairSoft_Dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-apt-get -y update
-apt-get -y upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y update
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
-apt-get -y install cmake cmake-data g++ gcc gfortran build-essential make patch sed \
+DEBIAN_FRONTEND=noninteractive apt-get -y install cmake cmake-data g++ gcc gfortran build-essential make patch sed \
                    libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev \
                    ncurses-dev libcurl4-openssl-dev curl bzip2 gzip unzip tar subversion git xutils-dev flex \
                    bison lsb-release python-dev libc6-dev-i386 libxml2-dev wget libssl-dev
 
-apt-get -y clean && apt-get -y autoremove
+DEBIAN_FRONTEND=noninteractive apt-get -y clean
+DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
--- debian/script/cleanup.sh
+++ debian/script/cleanup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Clean up
-apt-get -y autoremove
-apt-get -y clean
+DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
+DEBIAN_FRONTEND=noninteractive apt-get -y clean
 
 # Remove temporary files
 rm -rf /tmp/*
@@ -10,3 +10,8 @@ rm -rf /tmp/*
 # Zero out free space
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+
+version_major=$(lsb_release -rs | cut -d. -f1)
+if [ $version_major -eq 7 ]; then
+  shutdown -hP now
+fi
