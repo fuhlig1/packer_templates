f064847bc94bb3e81200b2f7163982e22269ca56
Florian Uhlig <f.uhlig@gsi.de>
2015-11-20 13:06:08 +0100

    Distinguish between different fedora versions.

--- fedora/script/FairSoft_Dependencies.sh
+++ fedora/script/FairSoft_Dependencies.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
-yum -y update
+version=$(lsb_release -rs)
 
-yum -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
-               libX11-devel libXft-devel libXpm-devel libXext-devel \
-               libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
-               curl bzip2 libbz2-dev gzip unzip tar expat-devel \
-               subversion git flex bison imake redhat-lsb-core \
-               python-devel libxml2-devel wget openssl-devel curl-devel \
-               automake autoconf libtool
+if [ $version -le 21 ]; then
+
+  yum -y update
+
+  yum -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
+                 libX11-devel libXft-devel libXpm-devel libXext-devel \
+                 libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
+                 curl bzip2 libbz2-dev gzip unzip tar expat-devel \
+                 subversion git flex bison imake redhat-lsb-core \
+                 python-devel libxml2-devel wget openssl-devel curl-devel \
+                 automake autoconf libtool
+else
+  dnf -y update
+
+  dnf -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
+                 libX11-devel libXft-devel libXpm-devel libXext-devel \
+                 libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
+                 curl bzip2 libbz2-dev gzip unzip tar expat-devel \
+                 subversion git flex bison imake redhat-lsb-core \
+                 python-devel libxml2-devel wget openssl-devel curl-devel \
+                 automake autoconf libtool
+fi
