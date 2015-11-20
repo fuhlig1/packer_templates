cb29f910390f7adf00946944dfead7c1500c0711
Florian Uhlig <f.uhlig@gsi.de>
2015-11-20 11:39:15 +0100

    Add template for Fedora 23. Change scripts accordingly.

--- fedora/script/cleanup.sh
+++ fedora/script/cleanup.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-yum -y autoremove  
-yum -y clean all
-yum -y history new
+
+set -xv
+
+version=$(lsb_release -rs)
+
+if [ $version -le 21 ]; then
+  yum -y autoremove  
+  yum -y clean all
+  yum -y history new
+else   
+  dnf -y autoremove  
+  dnf -y clean all
+  dnf -y history new
+fi  
+  
 truncate -c -s 0 /var/log/yum.log
 
 # Remove temporary files
--- fedora/script/update.sh
+++ fedora/script/update.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
-# Update the box
-yum -y update
-yum -y install linux-headers-$(uname -r) redhat-lsb-core
+set -xv
 
-if [ "$build_type" = "desktop" ]; then
-  yum -y install @xfce
-fi
+version=$(lsb_release -rs)
+
+if [ $version -le 21 ]; then
+
+  # Update the box
+  yum -y update
+  yum -y install linux-headers-$(uname -r) redhat-lsb-core
+
+  if [ "$build_type" = "desktop" ]; then
+    yum -y install @xfce
+  fi
+else
+  # Update the box
+  dnf -y update
+  dnf -y install linux-headers-$(uname -r) redhat-lsb-core
+
+  if [ "$build_type" = "desktop" ]; then
+    dnf -y install @xfce
+  fi
+fi  
   
+set +xv
