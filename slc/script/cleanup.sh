6927ae519f89cb57d9900036b8f053a0cfff1c8a
Florian Uhlig <f.uhlig@gsi.de>
2015-11-23 17:16:16 +0100

    Correct scripts for SLC6. SLC7 still has to be tested.

--- slc/script/cleanup.sh
+++ slc/script/cleanup.sh
@@ -15,3 +15,7 @@ rm -rf /tmp/*
 # Zero out free space
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
+
+# Remove network rules file which will be created otherwise in the 
+# wrong way disabling ssh login completely
+rm /etc/udev/rules.d/70-persistent-net.rules
