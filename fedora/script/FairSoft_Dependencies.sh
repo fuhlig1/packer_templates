494522a442a4b5aa17f599b2c795ab255b0982e7
Florian Uhlig <f.uhlig@gsi.de>
2015-11-23 17:16:54 +0100

    Correct package name in dnf command.

--- fedora/script/FairSoft_Dependencies.sh
+++ fedora/script/FairSoft_Dependencies.sh
@@ -19,7 +19,7 @@ else
   dnf -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
                  libX11-devel libXft-devel libXpm-devel libXext-devel \
                  libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
-                 curl bzip2 libbz2-dev gzip unzip tar expat-devel \
+                 curl bzip2 bzip2-devel gzip unzip tar expat-devel \
                  subversion git flex bison imake redhat-lsb-core \
                  python-devel libxml2-devel wget openssl-devel curl-devel \
                  automake autoconf libtool
