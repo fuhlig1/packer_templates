051989375e235c2eb9f258be0bbd08ca77a068f1
Florian Uhlig <f.uhlig@gsi.de>
2015-11-27 09:45:43 +0100

    Install additional software from software collections.

--- slc/script/update.sh
+++ slc/script/update.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -xv
-
   # Update the box
   yum -y update
   yum -y install linux-headers-$(uname -r) redhat-lsb-core
@@ -9,5 +7,14 @@ set -xv
   if [ "$build_type" = "desktop" ]; then
     yum -y install @xfce
   fi
-  
-set +xv
+
+  # Install the needed software collections when on SLC6 
+  # Install newer python and dev tools (compiler + ...)
+  distribution=$(lsb_release -is)
+  version=$(lsb_release -rs | cut -f1 -d.)
+
+  if [ "$distribution$version" = "ScientificCERNSLC6" ]; then
+    wget http://linuxsoft.cern.ch/cern/scl/slc6-scl.repo
+    cp slc6-scl.repo /etc/yum.repos.d
+    yum -y install python27 devtoolset-3
+  fi
