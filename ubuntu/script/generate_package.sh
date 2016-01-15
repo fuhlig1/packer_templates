9616183a12019f76453dbbb12500c6ca8c56adb6
Florian Uhlig <f.uhlig@gsi.de>
2016-01-15 17:27:28 +0100

    New Template to create a debian package from the code in /opt/fairroot/..version..root_version.

---
+++ ubuntu/script/generate_package.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+fpm -s dir -t deb -n fairsoft_nov15p1 \
+-d "cmake" \
+-d "cmake-data" \
+-d "g++" \
+-d "gcc" \
+-d "gfortran" \
+-d "build-essential" \
+-d "make" \
+-d "patch" \
+-d "sed" \
+-d "libx11-dev" \
+-d "libxft-dev" \
+-d "libxext-dev" \
+-d "libxpm-dev" \
+-d "libxmu-dev" \
+-d "libglu1-mesa-dev" \
+-d "libgl1-mesa-dev" \
+-d "ncurses-dev" \
+-d "libcurl4-openssl-dev" \
+-d "curl" \
+-d "bzip2" \
+-d "gzip" \
+-d "unzip" \
+-d "tar" \
+-d "subversion" \
+-d "git" \
+-d "xutils-dev" \
+-d "flex" \
+-d "bison" \
+-d "lsb-release" \
+-d "python-dev" \
+-d "libc6-dev-i386" \
+-d "libxml2-dev" \
+-d "wget" \
+-d "libssl-dev" \
+-d "automake" \
+-d "autoconf" \
+-d "libtool" \
+ /opt/fairsoft
---
+++ ubuntu/script/install_fpm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DEBIAN_FRONTEND=noninteractive apt-get install -y ruby-dev gcc make
+gem install fpm
