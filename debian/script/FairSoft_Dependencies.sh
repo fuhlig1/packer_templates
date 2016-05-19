e23921884ab85301045921dfce5204f312cc1cc5
Florian Uhlig <f.uhlig@gsi.de>
2016-05-19 21:26:49 +0200

    Add more needed packages to dependencies.

--- debian/script/FairSoft_Dependencies.sh
+++ debian/script/FairSoft_Dependencies.sh
@@ -7,7 +7,7 @@ DEBIAN_FRONTEND=noninteractive apt-get -y install cmake cmake-data g++ gcc gfort
                    libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev \
                    ncurses-dev libcurl4-openssl-dev curl bzip2 gzip unzip tar subversion git xutils-dev flex \
                    bison lsb-release python-dev libc6-dev-i386 libxml2-dev wget libssl-dev \
-                   automake autoconf libtool
+                   automake autoconf libtool libbz2-dev libkrb5-dev
 
 DEBIAN_FRONTEND=noninteractive apt-get -y clean
 DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
