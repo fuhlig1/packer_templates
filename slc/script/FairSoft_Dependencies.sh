896621f411a5913f2a5e628ff2a69aee6b8e56f4
Florian Uhlig <f.uhlig@gsi.de>
2016-05-19 16:36:00 +0200

    New FairSoft dependencies for slc7.
    Change output vm name for slc7.
    Use new autoinstall script for opensuse 13.2 42.1

--- slc/script/FairSoft_Dependencies.sh
+++ slc/script/FairSoft_Dependencies.sh
@@ -6,7 +6,9 @@
   yum -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
                  libX11-devel libXft-devel libXpm-devel libXext-devel \
                  libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
-                 curl bzip2 libbz2-dev gzip unzip tar expat-devel \
+                 curl bzip2 bzip2-devel gzip unzip tar expat-devel \
                  subversion git flex bison imake redhat-lsb-core \
                  python-devel libxml2-devel wget openssl-devel curl-devel \
-                 automake autoconf libtool glibc-devel.i686
+                 automake autoconf libtool krb5-devel
+
+# libbz2-dev removed, bzip2-devel added, glibc-devel.i686
