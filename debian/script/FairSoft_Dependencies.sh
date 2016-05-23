ffa7f58b75896691a721224f45b94841562e4448
Florian Uhlig <f.uhlig@gsi.de>
2016-05-23 11:51:35 +0200

    Update dependencies.

--- debian/script/FairSoft_Dependencies.sh
+++ debian/script/FairSoft_Dependencies.sh
@@ -5,9 +5,10 @@ DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
 DEBIAN_FRONTEND=noninteractive apt-get -y install cmake cmake-data g++ gcc gfortran build-essential make patch sed \
                    libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev \
-                   ncurses-dev libcurl4-openssl-dev curl bzip2 gzip unzip tar subversion git xutils-dev flex \
-                   bison lsb-release python-dev libc6-dev-i386 libxml2-dev wget libssl-dev \
-                   automake autoconf libtool libbz2-dev libkrb5-dev
+                   ncurses-dev libcurl4-openssl-dev curl bzip2 libbz2-dev gzip unzip tar \
+		   subversion git xutils-dev flex bison lsb-release python-dev \
+		   libc6-dev-i386 libxml2-dev wget libssl-dev libkrb5-dev \
+                   automake autoconf libtool
 
 DEBIAN_FRONTEND=noninteractive apt-get -y clean
 DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
--- fedora/script/FairSoft_Dependencies.sh
+++ fedora/script/FairSoft_Dependencies.sh
@@ -7,20 +7,18 @@ if [ $version -le 21 ]; then
   yum -y update
 
   yum -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
-                 libX11-devel libXft-devel libXpm-devel libXext-devel \
-                 libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
-                 curl bzip2 libbz2-dev gzip unzip tar expat-devel \
-                 subversion git flex bison imake redhat-lsb-core \
-                 python-devel libxml2-devel wget openssl-devel curl-devel \
+                 libX11-devel libXft-devel libXpm-devel libXext-devel libXmu-devel mesa-libGLU-devel mesa-libGL-devel \
+                 ncurses-devel curl curl-devel bzip2 bzip2-devel gzip unzip tar \
+                 expat-devel subversion git flex bison imake redhat-lsb-core python-devel \
+                 libxml2-devel wget openssl-devel krb5-devel \
                  automake autoconf libtool
 else
   dnf -y update
 
   dnf -y install cmake gcc gcc-c++ gcc-gfortran make patch sed \
-                 libX11-devel libXft-devel libXpm-devel libXext-devel \
-                 libXmu-devel mesa-libGLU-devel mesa-libGL-devel ncurses-devel \
-                 curl bzip2 bzip2-devel gzip unzip tar expat-devel \
-                 subversion git flex bison imake redhat-lsb-core \
-                 python-devel libxml2-devel wget openssl-devel curl-devel \
+                 libX11-devel libXft-devel libXpm-devel libXext-devel libXmu-devel mesa-libGLU-devel mesa-libGL-devel \
+                 ncurses-devel curl curl-devel bzip2 bzip2-devel gzip unzip tar \
+		 expat-devel subversion git flex bison imake redhat-lsb-core python-devel \
+                 libxml2-devel wget openssl-devel krb5-devel \
                  automake autoconf libtool
 fi
