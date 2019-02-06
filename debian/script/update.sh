efebb5088efd5a20dece4887094fe92a73028578
Florian Uhlig <f.uhlig@gsi.de>
2019-02-06 13:40:22 +0100

    Add recipe for Debian9 base image

--- debian/script/update.sh
+++ debian/script/update.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # Update the box
+# make gcc perl are needed to build the kernel modules for the guest additions
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
-DEBIAN_FRONTEND=noninteractive apt-get -y install linux-headers-$(uname -r) lsb-release
+DEBIAN_FRONTEND=noninteractive apt-get -y install linux-headers-$(uname -r) lsb-release make gcc perl
 
 if [ "$build_type" = "desktop" ]; then
   DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
