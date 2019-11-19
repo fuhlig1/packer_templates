0be12af527e3be4f60d3de75c33e001216d5526d
Florian Uhlig <f.uhlig@gsi.de>
2019-11-19 10:13:55 +0100

    Revert last change of FairSoft dependencies
    Add cmake again since it is needed during the FairSoft configuration.

--- ubuntu/script/FairSoft_Dependencies.sh
+++ ubuntu/script/FairSoft_Dependencies.sh
@@ -3,18 +3,11 @@
 DEBIAN_FRONTEND=noninteractive apt-get -y update
 DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
 
-# Remove cmake from list to trigger build of FairSofts cmake
-DEBIAN_FRONTEND=noninteractive apt-get -y install g++ gcc gfortran build-essential make patch sed \
+DEBIAN_FRONTEND=noninteractive apt-get -y install cmake cmake-data g++ gcc gfortran build-essential make patch sed \
                    libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev \
                    ncurses-dev libcurl4-openssl-dev curl bzip2 gzip unzip tar subversion git xutils-dev flex \
                    bison lsb-release python-dev libc6-dev-i386 libxml2-dev wget libssl-dev \
                    automake autoconf libtool libbz2-dev libkrb5-dev sqlite3 libsqlite3-dev
-
-#DEBIAN_FRONTEND=noninteractive apt-get -y install cmake cmake-data g++ gcc gfortran build-essential make patch sed \
-#                   libx11-dev libxft-dev libxext-dev libxpm-dev libxmu-dev libglu1-mesa-dev libgl1-mesa-dev \
-#                   ncurses-dev libcurl4-openssl-dev curl bzip2 gzip unzip tar subversion git xutils-dev flex \
-#                   bison lsb-release python-dev libc6-dev-i386 libxml2-dev wget libssl-dev \
-#                   automake autoconf libtool libbz2-dev libkrb5-dev sqlite3 libsqlite3-dev
 
 DEBIAN_FRONTEND=noninteractive apt-get -y clean
 DEBIAN_FRONTEND=noninteractive apt-get -y autoremove
